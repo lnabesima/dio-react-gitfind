d27347e17c48cdc32f1b11fdac1a2a8253e72d34
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-04-05 15:30:40 -0300

    feat: add reusable Header component
    Introduced a Header component with custom styles to improve layout
    structure. Updated the global styles to include a new font family and
    preloaded necessary fonts in the index.html file.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>GitFind</h1>
+      </header>
+    </>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import github from '../../assets/background.png'
+import { Header } from '../../components/Header';
 
 function App() {
 
   return (
     <>
+      <Header />
       <div style={{zIndex: -1}}>
         <img src={github} alt="GitHub logo"/>
       </div>
